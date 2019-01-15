151e38e2c03e9b00efc1732c2af47b56b7720622
dece1ver <v.dece1verr@gmail.com>
2019-01-15 16:45:01 +0300

    fix

--- superzalupa2.py
+++ superzalupa2.py
@@ -17,7 +17,7 @@ if not os.path.isfile('settings.ini'):
     logging.info('Settings file not found, creating a new one.')
     config = configparser.ConfigParser()
     config['SETTINGS'] = {'FastScanCheckboxState': 'False',
-                          'IgnoredFiles': '.PBG, PYC, .PY, .PYW, .KV, .UI, .MP3, .FLAC, .WAV, .OGG, .JPG, .JPEG, .BMP, .ICO, .TIFF, .JPE, .OXPS, .PSD, .PNG, .GIF, .MPEG, .MP4, .WEBM, .WMA, .FLV, .MOV, 3GP, .AVI, .VOB, .EXE, .RAR, .ZIP, .7Z, .MSI, .INSTALL, .APK, .XLS, .XLSX, .WPS, .FRW, .INI, .CFG, .DB, .DAT, .TMP, .DOC, .DOCX, .PDF, .DJVU, .FB2, .EPUB, .DB, .LNK, .URL, .HTML, .GP3, .GP4, .GP5, .GPX, .CDW, .FRW, .M3D, .KDW, .SPW, .A3D, .SYS, .HLP, .HTM, .HTML, .PPT, .COM'}
+                          'IgnoredFiles': '.PBG, .PYC, .PY, .PYW, .KV, .UI, .MP3, .FLAC, .WAV, .OGG, .JPG, .JPEG, .BMP, .ICO, .TIFF, .JPE, .OXPS, .PSD, .PNG, .GIF, .MPEG, .MP4, .WEBM, .WMA, .FLV, .MOV, .3GP, .AVI, .VOB, .EXE, .RAR, .ZIP, .7Z, .MSI, .INSTALL, .APK, .XLS, .XLSX, .WPS, .FRW, .INI, .CFG, .DB, .DAT, .TMP, .DOC, .DOCX, .PDF, .DJVU, .FB2, .EPUB, .DB, .LNK, .URL, .HTML, .GP3, .GP4, .GP5, .GPX, .CDW, .FRW, .M3D, .KDW, .SPW, .A3D, .SYS, .HLP, .HTM, .HTML, .PPT, .COM'}
 
     with open('settings.ini', 'w') as config_file:
         config.write(config_file)
